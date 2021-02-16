556e7741adba7d8cadadc6b8b2a8ac0dafddfccf
gam0022 <gam0022@gmail.com>
2021-02-16 15:38:02 +0900

    wip

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const chokidar = require('chokidar');
 const fs = require('fs');
+const Hjson = require('hjson');
 
 const settingsJson = '/mnt/c/Users/gam0022/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json';
 const shaderPath = '/mnt/c/Users/gam0022/Dropbox/windows-terminal/terminal/samples/PixelShaders';
+const winShaderPath = "C:\\Users\\gam0022\\Dropbox\\windows-terminal\\terminal\\samples\\PixelShaders\\";
+const shaderHlsl = 'Raymarching.hlsl';
 
 // One-liner for current directory
 chokidar.watch(shaderPath).on('change', (event, path) => {
   console.log(event, path);
-  const jsonObject = JSON.parse(fs.readFileSync(settingsJson, 'utf8'));
+  const jsonObject = Hjson.parse(fs.readFileSync(settingsJson, 'utf8'));
   console.log(jsonObject);
+  console.log(jsonObject.profiles['defaults']['experimental.pixelShaderPath']);
+
+  jsonObject.profiles['defaults']['experimental.pixelShaderPath'] = winShaderPath + "Invert.hlsl";
+  fs.writeFileSync(settingsJson, Hjson.stringify(jsonObject, {quotes: 'all', separator: true}));
+
+  setTimeout(function() {
+    jsonObject.profiles['defaults']['experimental.pixelShaderPath'] = winShaderPath + shaderHlsl;
+    fs.writeFileSync(settingsJson, Hjson.stringify(jsonObject, {quotes: 'all', separator: true}));
+  }, 500);
 });
