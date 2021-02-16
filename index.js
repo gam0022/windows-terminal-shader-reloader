01d1effb64b33ecca6cffb5e2da360ded5063d55
gam0022 <gam0022@gmail.com>
2021-02-16 16:11:20 +0900

    toggle copy

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const chokidar = require('chokidar');
 const fs = require('fs');
+const path = require('path');
+const chokidar = require('chokidar');
 const Hjson = require('hjson');
 
+const toWinPath = (str) => str.replace("/mnt/c/", "c:/");
+
 const settingsJson = '/mnt/c/Users/gam0022/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json';
-const shaderPath = '/mnt/c/Users/gam0022/Dropbox/windows-terminal/terminal/samples/PixelShaders';
-const winShaderPath = "C:\\Users\\gam0022\\Dropbox\\windows-terminal\\terminal\\samples\\PixelShaders\\";
-const shaderHlsl = 'Raymarching.hlsl';
+const shaderPath = '/mnt/c/Users/gam0022/Dropbox/windows-terminal/terminal/samples/PixelShaders/Raymarching.hlsl';
+const shaderPathCopy = path.join(path.dirname(shaderPath), "_Copy.hlsl");
+const shaderPathWin = toWinPath(shaderPath);
+const shaderPathCopyWin = toWinPath(shaderPathCopy);
+
+console.log("shaderPathCopy: " + shaderPathCopy);
+console.log("shaderPathWin: " + shaderPathWin);
+
+let toggle = true;
 
 // One-liner for current directory
 chokidar.watch(shaderPath).on('change', (event, path) => {
-  console.log(event, path);
   const jsonObject = Hjson.parse(fs.readFileSync(settingsJson, 'utf8'));
-  console.log(jsonObject);
-  console.log(jsonObject.profiles['defaults']['experimental.pixelShaderPath']);
-
-  jsonObject.profiles['defaults']['experimental.pixelShaderPath'] = winShaderPath + "Invert.hlsl";
-  fs.writeFileSync(settingsJson, Hjson.stringify(jsonObject, {quotes: 'all', separator: true}));
-
-  setTimeout(function() {
-    jsonObject.profiles['defaults']['experimental.pixelShaderPath'] = winShaderPath + shaderHlsl;
-    fs.writeFileSync(settingsJson, Hjson.stringify(jsonObject, {quotes: 'all', separator: true}));
-  }, 500);
+  fs.copyFile(shaderPath, shaderPathCopy, err => {
+    console.log("err: " + err);
+    console.log("toggle: " + toggle);
+    jsonObject.profiles['defaults']['experimental.pixelShaderPath'] = toggle ? shaderPathWin : shaderPathCopyWin;
+    toggle = !toggle;
+    fs.writeFileSync(settingsJson, Hjson.stringify(jsonObject, { quotes: 'all', separator: true }));
+  });
 });
